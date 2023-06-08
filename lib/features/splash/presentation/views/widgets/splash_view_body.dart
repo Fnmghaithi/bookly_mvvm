5ba0fbfa57b0ce4e90e072cfda6fb6cfee806890
Faisal <fnmghaithi@gmail.com>
2023-06-08 18:49:00 +0400

    refactor init state & navigate to home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_mvvm/constants.dart';
 import 'package:bookly_mvvm/core/utils/assets.dart';
+import 'package:bookly_mvvm/features/home/presentation/views/home_view.dart';
 import 'package:bookly_mvvm/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,6 +19,25 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   @override
   void initState() {
+    initSlidingAnimation();
+    navigateToHome();
+    super.initState();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.to(
+          () => const HomeView(),
+          transition: Transition.fade,
+          duration: kTransitionDuration,
+        );
+      },
+    );
+  }
+
+  void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
@@ -26,8 +48,6 @@ class _SplashViewBodyState extends State<SplashViewBody>
             .animate(animationController);
 
     animationController.forward();
-
-    super.initState();
   }
 
   @override
