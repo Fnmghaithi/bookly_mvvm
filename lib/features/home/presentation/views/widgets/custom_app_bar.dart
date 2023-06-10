a37bb1e24222295e9daa8b7fb5945d43b8dd6f08
Faisal <fnmghaithi@gmail.com>
2023-06-10 15:51:31 +0400

    added featured list view to home

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -8,7 +8,11 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.only(
+        left: 24,
+        right: 24,
+        bottom: 16,
+      ),
       child: Row(
         children: [
           Image.asset(
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -11,8 +11,31 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         children: const [
           CustomAppBar(),
-          CustomListViewItem(),
+          FeaturedBoxListView(),
         ],
+      ),
+    );
+  }
+}
+
+class FeaturedBoxListView extends StatelessWidget {
+  const FeaturedBoxListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 8.0,
+              vertical: 8.0,
+            ),
+            child: FeaturedListViewItem(),
+          );
+        },
       ),
     );
   }
