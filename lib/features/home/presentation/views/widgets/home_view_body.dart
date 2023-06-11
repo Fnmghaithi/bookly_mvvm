c62fd5d7173a6fd53be2fcbd7df42bb3f2ab5fb2
Faisal <fnmghaithi@gmail.com>
2023-06-11 15:30:27 +0400

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_mvvm/core/utils/assets.dart';
 import 'package:bookly_mvvm/core/utils/styles.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -21,8 +22,39 @@ class HomeViewBody extends StatelessWidget {
               'Best Seller',
               style: Styles.titleMedium,
             ),
+            BestSellerListViewItem(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
+        ],
       ),
     );
   }
